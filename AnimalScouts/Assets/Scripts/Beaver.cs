4d490c68b34e9fcee8334013b232fa1cfc13b313
Zeke <49454258+RunningMonkeys@users.noreply.github.com>
2020-04-21 15:47:00 -0500

    all animals move properly

--- AnimalScouts/Assets/Scripts/Beaver.cs
+++ AnimalScouts/Assets/Scripts/Beaver.cs
@@ -9,10 +9,19 @@ public class Beaver : Piece
 	{
 		bool[,] r = new bool[xSize,ySize];
 		Piece p;
+		Tile t;
 		//right
 		for(int i = 1; i <= numMoves && CurrentX+i< xSize; i++)
 		{
 			p = BoardManager.Instance.Pieces[CurrentX+i,CurrentY];
+			t = BoardManager.Instance.tileGrid[CurrentX+i,CurrentY];
+			if(t == null)
+			{
+				Debug.Log("Tile Not Found");
+			}
+			else if(t.tileType == 4) {
+				break;
+			}
 			if(p == null)
 			{
 				r[CurrentX+i,CurrentY] = true;
@@ -27,6 +36,14 @@ public class Beaver : Piece
 		for(int i = 1; i <= numMoves && CurrentX-i >=0; i++)
 		{
 			p = BoardManager.Instance.Pieces[CurrentX-i,CurrentY];
+			t = BoardManager.Instance.tileGrid[CurrentX-i,CurrentY];
+			if(t == null)
+			{
+				Debug.Log("Tile Not Found");
+			}
+			else if(t.tileType == 4) {
+				break;
+			}
 			if(p == null)
 			{
 				r[CurrentX-i,CurrentY] = true;
@@ -41,6 +58,14 @@ public class Beaver : Piece
 		for(int i = 1; i <= numMoves && CurrentY+i < ySize; i++)
 		{
 			p = BoardManager.Instance.Pieces[CurrentX,CurrentY+i];
+			t = BoardManager.Instance.tileGrid[CurrentX,CurrentY+i];
+			if(t == null)
+			{
+				Debug.Log("Tile Not Found");
+			}
+			else if(t.tileType == 4) {
+				break;
+			}
 			if(p == null)
 			{
 				r[CurrentX,CurrentY+i] = true;
@@ -55,6 +80,14 @@ public class Beaver : Piece
 		for(int i = 1; i <= numMoves && CurrentY - i >= 0; i++)
 		{
 			p = BoardManager.Instance.Pieces[CurrentX,CurrentY-i];
+			t = BoardManager.Instance.tileGrid[CurrentX,CurrentY-i];
+			if(t == null)
+			{
+				Debug.Log("Tile Not Found");
+			}
+			else if(t.tileType == 4) {
+				break;
+			}
 			if(p == null)
 			{
 				r[CurrentX,CurrentY-i] = true;
--- AnimalScouts/Assets/Scripts/Lizard.cs
+++ AnimalScouts/Assets/Scripts/Lizard.cs
@@ -8,10 +8,19 @@ public class Lizard : Piece
 	{
 		bool[,] r = new bool[xSize,ySize];
 		Piece p;
+		Tile t;
 		//right
 		for(int i = 1; i <= numMoves && CurrentX+i< xSize; i++)
 		{
 			p = BoardManager.Instance.Pieces[CurrentX+i,CurrentY];
+			t = BoardManager.Instance.tileGrid[CurrentX+i,CurrentY];
+			if(t == null)
+			{
+				Debug.Log("Tile Not Found");
+			}
+			else if(t.tileType == 2) {
+				break;
+			}
 			if(p == null)
 			{
 				r[CurrentX+i,CurrentY] = true;
@@ -26,6 +35,14 @@ public class Lizard : Piece
 		for(int i = 1; i <= numMoves && CurrentX-i >=0; i++)
 		{
 			p = BoardManager.Instance.Pieces[CurrentX-i,CurrentY];
+			t = BoardManager.Instance.tileGrid[CurrentX-i,CurrentY];
+			if(t == null)
+			{
+				Debug.Log("Tile Not Found");
+			}
+			else if(t.tileType == 2) {
+				break;
+			}
 			if(p == null)
 			{
 				r[CurrentX-i,CurrentY] = true;
@@ -40,6 +57,14 @@ public class Lizard : Piece
 		for(int i = 1; i <= numMoves && CurrentY+i < ySize; i++)
 		{
 			p = BoardManager.Instance.Pieces[CurrentX,CurrentY+i];
+			t = BoardManager.Instance.tileGrid[CurrentX,CurrentY+i];
+			if(t == null)
+			{
+				Debug.Log("Tile Not Found");
+			}
+			else if(t.tileType == 2) {
+				break;
+			}
 			if(p == null)
 			{
 				r[CurrentX,CurrentY+i] = true;
@@ -54,6 +79,14 @@ public class Lizard : Piece
 		for(int i = 1; i <= numMoves && CurrentY - i >= 0; i++)
 		{
 			p = BoardManager.Instance.Pieces[CurrentX,CurrentY-i];
+			t = BoardManager.Instance.tileGrid[CurrentX,CurrentY-i];
+			if(t == null)
+			{
+				Debug.Log("Tile Not Found");
+			}
+			else if(t.tileType == 2) {
+				break;
+			}
 			if(p == null)
 			{
 				r[CurrentX,CurrentY-i] = true;
